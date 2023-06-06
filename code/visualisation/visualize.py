86c5177c9d569c911cc3088255d5426d9eee3a9f
Alaya Storm <alayastorm@hotmail.nl>
2023-06-06 13:40:40 +0200

    update visualize function

--- code/visualisation/visualize.py
+++ code/visualisation/visualize.py
@@ -5,12 +5,12 @@ def visualize(self):
     pos_x = []
     pos_y = []
     color = []
-    for aminoacid in self.sequence:
-        pos_x.append(aminoacid.pos_x)
-        pos_y.append(aminoacid.pos_y)
+    for aminoacid in self.sequence_list:
+        pos_x.append(aminoacid.location_x)
+        pos_y.append(aminoacid.location_y)
         color.append(aminoacid.color)
 
-    plt.scatter(pos_x, pos_y, color)
-    plt.plot(pos_x, pos_y, 'black-')
-    plt.grid(True)
+    plt.plot(pos_x, pos_y, c = 'black', linestyle = '-')
+    plt.scatter(pos_x, pos_y, c = color, markersize = 12)
+    plt.grid(True, linestyle = '--')
     plt.show()
