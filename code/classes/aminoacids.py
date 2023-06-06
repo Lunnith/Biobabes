5e7b466551c152dd8eaba60a65ee5f19a032e032
Alaya Storm <alayastorm@hotmail.nl>
2023-06-06 16:09:05 +0200

    visualize changes

--- code/classes/aminoacids.py
+++ code/classes/aminoacids.py
@@ -111,18 +111,18 @@ class Protein():
     
 
     def visualize(self):
-    pos_x = []
-    pos_y = []
-    color = []
-    for aminoacid in self.sequence_list:
-        pos_x.append(aminoacid.location_x)
-        pos_y.append(aminoacid.location_y)
-        color.append(aminoacid.color)
+        pos_x = []
+        pos_y = []
+        color = []
+        for aminoacid in self.sequence_list:
+            pos_x.append(aminoacid.location_x)
+            pos_y.append(aminoacid.location_y)
+            color.append(aminoacid.color)
 
-    plt.plot(pos_x, pos_y, c = 'black', linestyle = '-')
-    plt.scatter(pos_x, pos_y, c = color, markersize = 12)
-    plt.grid(True, linestyle = '--')
-    plt.show()
+        plt.plot(pos_x, pos_y, c = 'black', linestyle = '-', linewidth = 0.7, zorder = 1)
+        plt.scatter(pos_x, pos_y, c = color, s = 50, zorder = 2)
+        plt.grid(True, linestyle = '--')
+        plt.show()
         
 
 
