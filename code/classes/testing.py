bf47bf8f33d270e82339625fc11ebd13e8027738
Sofie Perizonius <sofie.perizonius@gmail.com>
2023-06-07 13:13:30 +0200

    opslaan

--- code/classes/testing.py
+++ code/classes/testing.py
@@ -7,10 +7,6 @@ protein.create_bonds()
 for acid in protein.sequence_list:
     print(f"Class = {acid}, Step = {acid.step}, Coordinates = ({acid.location_x}, {acid.location_y})")
 
-##################################3
-
-
-
 protein.visualize()
 protein.create_output()
 
