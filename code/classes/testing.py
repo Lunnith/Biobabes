0260225307d1ff1b5569d5a267bfe1242287c254
Lunnith <129735221+Lunnith@users.noreply.github.com>
2023-06-07 13:55:38 +0200

    bug fix

--- code/classes/testing.py
+++ code/classes/testing.py
@@ -4,6 +4,7 @@ from protein import Protein
 protein = Protein("HPCHP")
 protein.create_bonds()
 # print(protein.sequence_list)
+protein.check_interactions(protein.sequence_list[3])
 
 for acid in protein.sequence_list:
     print(f"Class = {acid}, Step = {acid.step}, Coordinates = ({acid.location_x}, {acid.location_y})")
