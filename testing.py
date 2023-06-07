d48e2e7c4805895d5c67267fb6c145a85d048226
Alaya Storm <alayastorm@hotmail.nl>
2023-06-07 15:05:39 +0200

    langer protein

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 from code.classes.protein import Protein
 from code.visualisation.visualize import visualize
 
-proteinA = Protein("HPCHP")
+proteinA = Protein("HPCHPCHH")
 proteinA.create_bonds()
 # print(protein.sequence_list)
 
