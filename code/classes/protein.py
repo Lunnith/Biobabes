4f7b924fcc9b13987e0d7269f055cba7cdfc2352
Sofie Perizonius <sofie.perizonius@gmail.com>
2023-06-07 14:11:09 +0200

    fetch

--- code/classes/protein.py
+++ code/classes/protein.py
@@ -114,21 +114,6 @@ class Protein():
         for aminoacid in self.sequence_list:
             print(f'{aminoacid.type},{aminoacid.step}')
         print(f'score,{self.score}')
-    
-
-    def visualize(self):
-        pos_x = []
-        pos_y = []
-        color = []
-        for aminoacid in self.sequence_list:
-            pos_x.append(aminoacid.location_x)
-            pos_y.append(aminoacid.location_y)
-            color.append(aminoacid.color)
-
-        plt.plot(pos_x, pos_y, c = 'black', linestyle = '-', linewidth = 0.7, zorder = 1)
-        plt.scatter(pos_x, pos_y, c = color, s = 50, zorder = 2)
-        plt.grid(True, linestyle = '--')
-        plt.show()
 
 
 class Aminoacid():
--- code/classes/testing.py
+++ code/classes/testing.py
@@ -9,8 +9,7 @@ protein.check_interactions(protein.sequence_list[3])
 
 for acid in protein.sequence_list:
     print(f"Class = {acid}, Step = {acid.step}, Coordinates = ({acid.location_x}, {acid.location_y})")
-
-protein.visualize()
+    
 protein.create_output()
 
 
