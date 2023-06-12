3954c6d8cb37bd75f9a2ff05e2e9aa80a7e0c2c5
Sofie Perizonius <sofie.perizonius@gmail.com>
2023-06-12 10:08:15 +0200

    got random_assignment to work
    Co-authored-by: Sofie Perizonius <sofie.perizonius@gmail.com>
    Co-authored-by: Lunnith <129735221+Lunnith@users.noreply.github.com>

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -5,25 +5,26 @@ def random_assignment(protein, directions):
     """
     Randomly assign direction to each bond within the protein.
     """
-    folded_protein = copy.deepcopy(protein)
+    for i in range(len(protein.sequence)):
+        protein.add_aminoacid(protein.sequence[i])
 
-    for i in range(len(folded_protein.sequence)):
-        folded_protein.add_aminoacid(folded_protein.sequence[i])
-
-        acid = folded_protein.sequence_list[i]
+        acid = protein.sequence_list[i]
         
         if i == 0:
             acid.location_x = 0
             acid.location_y = 0
-            folded_protein.used_coordinates.add((acid.location_x, acid.location_y))
+            protein.used_coordinates.add((acid.location_x, acid.location_y))
 
         if i != 0:
             tried_directions = set()
 
             while acid.location_valid == False and tried_directions.difference(directions) == set():
                 direction = random.choice(tuple(directions))
-                folded_protein.create_bond(acid, folded_protein.sequence_list[i - 1], direction)
+                protein.create_bond(acid, protein.sequence_list[i - 1], direction)
 
                 tried_directions.add(direction)
 
-    return folded_protein
+        if i > 2:
+            acid.check_interactions(protein)
+
+    return protein
