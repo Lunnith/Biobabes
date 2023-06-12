bb245ed4a2069c67a8b146e5305bd61349c007f3
Lunnith <129735221+Lunnith@users.noreply.github.com>
2023-06-12 14:38:17 +0200

    3D structures implemented
    Co-authored-by: sperizonius <sperizonius@users.noreply.github.com>
    Co-authored-by: AEMStorm <AEMStorm@users.noreply.github.com>

---
+++ code/classes/aminoacid.py
@@ -0,0 +1,78 @@
+import math
+
+class Aminoacid():
+    """
+    A class to represent an aminoacid.
+    ...
+    Attributes:
+    -----------
+    location_x: int
+        the x coordinate of the position of the aminoacid after folding
+    location_y: int
+        the y coordinate of the position of the aminoacid after folding
+    step: int
+        the direction of the folding for this acid
+    type: string
+        polar, hydrophobic or cysteine aminoacid
+    color: string
+        polar = 'royalgreen'
+        hydrophobic = 'red'
+        cysteine = 'limegreen'
+
+    Methods:
+    -----------
+    distance():
+        calculate distance between one aminoacid and another
+    check_interactions():
+        check for possible other interacting aminoacids
+    """
+    def __init__(self, type):
+     
+        self.location = [None, None, None]
+        self.neighbour1 = None
+
+        self.step = 0
+        self.type = type
+
+        self.location_valid = False
+
+        # initialize color based on type
+        if self.type == 'P':
+            self.color = 'b'
+        
+        elif self.type == 'H':
+            self.color = 'r'
+        
+        elif self.type == 'C':
+            self.color = 'g'
+    
+    def distance(self, other):
+        """
+        Calculate euclidian distance between this aminoacid and another aminoacid.
+        """
+        return math.sqrt((self.location[0] - other.location[0])**2 + (self.location[1] - other.location[1])**2 + (self.location[2] - other.location[2])**2)
+   
+
+    def check_interactions(self, protein):
+        """
+        Check surrounding of aminoacid for other aminoacids, if they are present, check type
+        and change score according to the interaction type.
+        """
+        # loop through all potential interactors for this aminoacid
+        for potential_interactor in protein.sequence_list:
+
+            # only check for interactions with other acid than bound amino acids and interactors in close proximity
+            if potential_interactor != self.neighbour1 and self.distance(potential_interactor) == 1:
+                
+                # change score according to which interaction is happening
+                types = set((('H', 'H'), ('C', 'H'), ('H', 'C'), ('C', 'C')))
+                interaction_combination = tuple((self.type, potential_interactor.type))
+
+                if interaction_combination in types:
+                    
+                    if interaction_combination == ('C', 'C'):
+                        protein.score -= 5
+                        protein.cc_bonds.append((self.location, potential_interactor.location))
+                    else:
+                        protein.score -= 1
+                        protein.hh_ch_bonds.append((self.location, potential_interactor.location))
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from code.classes.protein import Protein
+from code.classes.aminoacid import Aminoacid
 from code.visualization.visualize import *
 from code.algorithms.randomise import *
 
@@ -7,12 +8,11 @@ if __name__ == "__main__":
     # initialize protein object and directions
     sequence = 'PPCHHPPCHPPPPCHHHHCHHPPHHPPPPHHPPHPP'
     protein = Protein(sequence)
-    directions_2D = set(((1, 0, 1), (-1, 0, -1), (0, 1, 2), (0, -1, -2)))
 
     # fold protein randomly and create output
-    folded_protein, score_list = random_reassignment(protein, directions_2D, 100)
+    folded_protein, score_list = random_reassignment(protein, 3, 100)
     folded_protein.create_output()
 
     # visualize protein
     visualize_scores(score_list)
-    visualize_protein(folded_protein)
+    visualize_protein(folded_protein, 3)
