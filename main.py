6de55b0f3e8513b66a582896e30eff9b1dd365f7
Sofie Perizonius <sofie.perizonius@gmail.com>
2023-06-13 10:02:57 +0200

    wrote baseline

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from code.classes.protein import Protein
-from code.classes.aminoacid import Aminoacid
 from code.visualization.visualize import *
 from code.algorithms.randomise import *
 
 if __name__ == "__main__":
 
     # initialize protein object and directions
-    sequence = 'PPCHHPPCHPPPPCHHHHCHHPPHHPPPPHHPPHPP'
+    sequence = "HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH"
     protein = Protein(sequence)
 
     # fold protein randomly and create output
-    folded_protein, score_list = random_reassignment(protein, 3, 100)
+    folded_protein, score_list = random_reassignment(protein, 2, 100000)
     folded_protein.create_output()
 
     # visualize protein
     visualize_scores(score_list)
-    visualize_protein(folded_protein, 3)
+    visualize_protein(folded_protein, 2)
