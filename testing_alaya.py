9fedb7cae5a4719586ad4c0b5f0c73c1f2a8dbd8
Lunnith <129735221+Lunnith@users.noreply.github.com>
2023-06-20 16:18:18 +0200

    Visualisation done

--- testing_alaya.py
+++ testing_alaya.py
@@ -7,6 +7,8 @@ from code.algorithms import greedy as gr
 import random
 import copy
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 from operator import add
 
 
@@ -27,21 +29,26 @@ if __name__ == "__main__":
     sequence = "HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH"
     list_scores = []
     splits = [1, 2, 3, 4, 5]
+    iterations = 50
+
+    all_scores = []
+    score_dict = {}
     for split in splits:
         print(split)
-        for i in range(5):
+        score_dict[split] = []
+        for i in range(iterations):
             protein = Protein(sequence)
             greedy_test = gr.Greedy(protein, 3, splits = split)
             greedy_test.run_k()
-            list_scores.append([split, greedy_test.protein.score])
-    #visualize_protein(protein, 3)
-    print(list_scores)
+            all_scores.append(greedy_test.protein.score)
+            score_dict[split].append(greedy_test.protein.score)
 
-    dict_scores = {}
-    for split in splits:
-        dict_scores[split] = []
-    
-    for run in list_scores:
-        dict_scores[run[0]].append(run[1])
-    
-    print(dict_scores)
+    for split in score_dict:
+        sns.histplot(score_dict[split], kde=True, discrete=True, label=split)
+
+    plt.legend(title="Splits")
+    plt.title("Greedy", fontweight='bold')
+    plt.xlabel("Score", loc='right')
+    plt.ylabel("Frequency", loc='top')
+
+    plt.show()
