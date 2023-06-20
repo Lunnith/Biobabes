050d55f08c9c3c75500be42c569f48d3e0c31bc4
Lunnith <129735221+Lunnith@users.noreply.github.com>
2023-06-20 15:24:47 +0200

    Messing with timing tests

--- testing_luna.py
+++ testing_luna.py
@@ -1,6 +1,7 @@
 from code.classes.protein import Protein
 from code.visualization.visualize import *
 from code.algorithms.hill_climber import *
+from code.algorithms.randomise import *
 import time
 
 #NOTE TO SELF: 2D version is buggy
@@ -9,63 +10,11 @@ import time
 sequence = "HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH"
 # sequence = "HCPHCH"
 
-#Creating time thingy
-# def algorithm_timing(sequence, algorithm, *kwargs):
-def algorithm_timing(sequence):
-    """
-    
-    """
-    total_start = time.time()
-    times = {}
-    for seq_length in range(len(sequence)):
-        times[seq_length] = None
 
-    for seq_length in range(len(sequence)):
-        if seq_length <2:
-            continue
-        protein = Protein(sequence[:seq_length+1])
-        hill_climber = Hill_climber(protein)
 
-        start = time.time()
-        hill_climber.run_n_iterations(protein, 500, 1)
-        end = time.time()
-        times[seq_length] = (end-start)
-        
-        #Make safety net for exponential algorithms
-        if end-start > 1200: #That's 20 minutes
-            break
-
-    total_end = time.time()
-    print("Total runtime:", total_end-total_start)
-    plt.plot(times.keys(), times.values())
-    # plt.title("Hill_climber")
-    # plt.show()
-
-# algorithm_timing(sequence)
-
-def timing_for_multiple_n(sequence, max_n=10):
-    """
-    
-    """
-    start = time.time()
-    for n in range(1, max_n):
-        algorithm_timing(sequence)
-    end = time.time()
-    print("Runtime multiple n:", end-start)
-
-    plt.title("Hill_climber")
-    plt.legend(range(1, max_n))
-    plt.xlim(left=0, right=len(sequence))
-    # plt.ylim()
-    plt.title("Hill-climber", fontweight='bold')
-    plt.xlabel("Length protein-sequence", loc='right')
-    plt.ylabel("Time per 500 iterations\n(in seconds)", loc='top')
-    plt.legend(range(1, n+1), title="Minimum amount of bonds changed per iteration", ncol=n//2)
-    plt.show()
-
-timing_for_multiple_n(sequence)
-
-# # protein = Protein("HPPCCHPPH")
+protein = Protein("HPPCCHPPH")
+hill_climber = Hill_climber(protein, 3)
+folded_protein = hill_climber.protein
 
 # print("Starting score =", hill_climber.lowest_score)
 # # hill_climber.change_one_bond(protein)
@@ -76,8 +25,8 @@ timing_for_multiple_n(sequence)
 # folded_protein, best_score = hill_climber.run_n_iterations(protein, 1000, 1)
 # print(best_score)
 # visualize_protein(folded_protein, 3)
-# if folded_protein.score > -20:
-#     best_protein = hill_climber.experiment(folded_protein, 500, 50, max_n=10)
-#     visualize_protein(best_protein, 3)
-# else: print("Score too good:", folded_protein.score)
+if folded_protein.score > -20:
+    best_protein = hill_climber.experiment(folded_protein, 50, 5, max_n=5)
+    visualize_protein(best_protein, 3)
+else: print("Score too good:", folded_protein.score)
 
---
+++ timing.py
@@ -0,0 +1,105 @@
+from code.classes.protein import Protein
+from code.visualization.visualize import *
+from code.algorithms.hill_climber import *
+from code.algorithms.randomise import *
+from operator import add
+import matplotlib.pyplot as plt
+import random
+import time
+
+sequence = "HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH"
+
+
+######################## Hill_climber
+#Creating time thingy
+# def algorithm_timing(sequence, algorithm, *kwargs):
+def algorithm_timing(sequence):
+    """
+    
+    """
+    total_start = time.time()
+    times = {}
+    for seq_length in range(len(sequence)):
+        times[seq_length] = None
+
+    for seq_length in range(len(sequence)):
+        if seq_length <2:
+            continue
+        protein = Protein(sequence[:seq_length+1])
+        hill_climber = Hill_climber(protein)
+
+        start = time.time()
+        hill_climber.run_n_iterations(protein, 500, 1)
+        end = time.time()
+        times[seq_length] = (end-start)
+        
+        #Make safety net for exponential algorithms
+        if end-start > 1200: #That's 20 minutes
+            break
+
+    total_end = time.time()
+    print("Total runtime:", total_end-total_start)
+    plt.plot(times.keys(), times.values())
+    # plt.title("Hill_climber")
+    # plt.show()
+
+# algorithm_timing(sequence)
+
+def timing_for_multiple_n(sequence, max_n=10):
+    """
+    
+    """
+    start = time.time()
+    for n in range(1, max_n):
+        algorithm_timing(sequence)
+    end = time.time()
+    print("Runtime multiple n:", end-start)
+
+    plt.title("Hill_climber")
+    plt.legend(range(1, max_n))
+    plt.xlim(left=0, right=len(sequence))
+    # plt.ylim()
+    plt.title("Hill-climber", fontweight='bold')
+    plt.xlabel("Length protein-sequence", loc='right')
+    plt.ylabel("Time per 500 iterations\n(in seconds)", loc='top')
+    plt.legend(range(1, n+1), title="Minimum amount of bonds changed per iteration", ncol=n//2)
+    plt.show()
+
+# timing_for_multiple_n(sequence)
+
+################################ Randomise
+def algorithm_timing(sequence):
+    """
+
+    """
+    total_start = time.time()
+    times = {}
+    for seq_length in range(len(sequence)):
+        times[seq_length] = None
+
+    for seq_length in range(len(sequence)):
+        if seq_length <2:
+            continue
+        protein = Protein(sequence[:seq_length+1])
+        
+        start = time.time()
+        folded_protein, score_list = random_reassignment(protein, 3, k=50)
+        end = time.time()
+        times[seq_length] = (end-start)
+
+        #Make safety net for exponential algorithms
+        if end-start > 1200: #That's 20 minutes
+            break
+
+    total_end = time.time()
+    print("Total runtime:", total_end-total_start)
+    plt.plot(times.keys(), times.values())
+    plt.xlim(left=0, right=len(sequence))
+
+    plt.title("Randomise", fontweight='bold')
+    plt.xlabel("Length protein-sequence", loc='right')
+    plt.ylabel("Time per 50 iterations\n(in seconds)", loc='top')
+    plt.show()
+
+
+algorithm_timing(sequence)
