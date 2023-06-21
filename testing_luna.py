6c6b34a4daaa4a2e3e55724a6f9eb67e69127ec2
Lunnith <129735221+Lunnith@users.noreply.github.com>
2023-06-21 19:58:46 +0200

    Bug came back, now fixed. Also turned off prints

--- testing_luna.py
+++ testing_luna.py
@@ -16,18 +16,18 @@ protein = Protein(sequence)
 hill_climber = Hill_climber(protein, 3)
 folded_protein = hill_climber.protein
 
-print("Starting score =", hill_climber.lowest_score)
+# print("Starting score =", hill_climber.lowest_score)
 folded_protein, index = hill_climber.change_bond(protein)
 folded_protein = hill_climber.refold(protein, index)
 # visualize_protein(folded_protein, 3)
-folded_protein = hill_climber.refold_into_valid_state(protein)
+# folded_protein = hill_climber.refold_into_valid_state(protein)
 
 # visualize_protein(folded_protein, 3)
 
 
 
 folded_protein, best_score, scores, improvement = hill_climber.run_n_iterations(protein, 10, 1)
-print(best_score)
+# print(best_score)
 # visualize_protein(folded_protein, 3)
 if folded_protein.score > -20:
     best_protein = hill_climber.experiment(folded_protein, 5, 3, max_n=5)
