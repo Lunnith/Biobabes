4d1183c95388a46aa5e920f8c85f10f746b697ae
Alaya Storm <alayastorm@hotmail.nl>
2023-06-23 14:54:34 +0200

    experiment greedy

--- testing_alaya.py
+++ testing_alaya.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from operator import add
+import time
 
 
 if __name__ == "__main__":
@@ -20,9 +21,9 @@ if __name__ == "__main__":
     #sequence = "HPCPHHPCCHPHCCHPHHHCCCPPHCPHCPHCCCPHHHCPPCCHPCCCHPHCCHHHHPCCCPPPCHP"
     #sequence = "HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH"
     #sequence = "HPCPHHPCCHPHCCHPHHHCCCPPHCPHCPHCCCPHHHCPPCCHPCCCHPHCCHHHHPCCCPPPCH"
-    #sequence = "HCPHPHPHCHHHHPCCPPHPPPHPPPPCPPPHPPPHPHHHHCHPHPHPHH"
+    # sequence = "HCPHPHPHCHHHHPCCPPHPPPHPPPPCPPPHPPPHPHHHHCHPHPHPHH"
     # protein = Protein(sequence)
-    # greedy_test = gr.Greedy(protein, 3, splits = 4,  before = 0)
+    # greedy_test = gr.Greedy(protein, 3, splits = 1,  before = 0)
     # greedy_test.run()
     # visualize_protein(protein, 3)
 
@@ -30,29 +31,61 @@ if __name__ == "__main__":
     #sequence = "HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH"
     #sequence = "HHPHPHPHPHHHHPHPPPHPPPHPPPPHPPPHPPPHPHHHHPHPHPHPHH"
     sequence = "HCPHPHPHCHHHHPCCPPHPPPHPPPPCPPPHPPPHPHHHHCHPHPHPHH"
-    best_score = 1
-    list_best_scores = []
-    list_scores = []
-    dict_splits = {1: 10000}
-                   #, 2: 1000, 3: 300, 4: 20, 5: 10}
+    df_exp_greedy_2000i = pd.DataFrame()
+    df_exp_greedy_2000i_total = pd.DataFrame()
+
+    splits = []
+    splits_total = []
+    scores = []
+    best_scores = []
+    befores = []
+    total_time = []
+
+    dict_splits = {1: 2000, 2: 1000, 3: 667, 4: 500, 5: 400}
     for key, value in dict_splits.items():
+
+        start_time = time.time()
+        best_score = 1
+        print(key)
+
         for before in range(key):
+            print(before)
+
             for i in range(value):
                 protein = Protein(sequence)
                 greedy_test = gr.Greedy(protein, 3, splits = key, before = before)
                 greedy_test.run()
-                list_scores.append([key, greedy_test.protein.score])
+                scores.append(greedy_test.protein.score)
+                befores.append(before)
+                splits.append(key)
                 if greedy_test.protein.score < best_score:
                     best_protein = copy.deepcopy(greedy_test.protein)
                     best_score = best_protein.score
-        list_best_scores.append([key, best_protein.score])
+
+        end_time = time.time()
+
+        total_time.append(end_time - start_time)
+        splits_total.append(key)
+        best_scores.append(best_score)
+
     print(best_protein)
     print(best_score)
-    print(list_scores)
-    print(list_best_scores)
+    
+    df_exp_greedy_2000i['split_numbers'] = splits
+    df_exp_greedy_2000i['scores'] = scores
+    df_exp_greedy_2000i['befores'] = befores
+
+    df_exp_greedy_2000i_total['split_number'] = splits_total
+    df_exp_greedy_2000i_total['best_scores'] = best_scores
+    df_exp_greedy_2000i_total['total_time'] = total_time
 
     visualize_protein(best_protein, 3)
 
+    print(df_exp_greedy_2000i.head())
+    df_exp_greedy_2000i.to_csv(path_or_buf=r'C:\Users\alaya\Documents\Data_Greedy\df_exp_greedy_2000i')
+
+    print(df_exp_greedy_2000i_total.head())
+    df_exp_greedy_2000i_total.to_csv(path_or_buf=r'C:\Users\alaya\Documents\Data_Greedy\df_exp_greedy_2000i_total')
 
     # for split in score_dict:
     #     sns.histplot(score_dict[split], kde=True, discrete=True, label=split)
