b0c2ae140a4124dda186f8ab075d4cdb8a701e98
Lunnith <129735221+Lunnith@users.noreply.github.com>
2023-06-26 12:32:00 +0200

    experimenting

---
+++ code/experiments/hill_climber_vs_sim_annealing.py
@@ -0,0 +1,29 @@
+from ..classes.protein import Protein
+from ..classes.aminoacid import Aminoacid
+from ..algorithms.hill_climber import Hill_climber
+from ..algorithms.simulated_annealing import SimulatedAnnealing
+from ..visualization.visualize import *
+
+sequence = "HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH"
+
+# protein = Protein(sequence)
+# hill_climber = Hill_climber(protein, prints=True, dimensions=3)
+# folded_protein, best_score, scores, improvement = hill_climber.run_i_iterations(protein, 10, 1)
+# # print(best_score)
+# # visualize_protein(folded_protein, 3)
+# protein = Protein(sequence)
+# if folded_protein.score > -20:
+#     best_protein = hill_climber.experiment(protein, 500, 2, max_n=10)
+#     visualize_protein(best_protein, 3)
+# else: print("Score too good:", protein.score)
+
+protein = Protein(sequence)
+sim_annealing = SimulatedAnnealing(protein, start_n=10, dimensions=3, prints=True)
+folded_protein, best_score, scores, improvement = sim_annealing.run_i_iterations(protein, 100, 10)
+# print(best_score)
+# visualize_protein(folded_protein, 3)
+# protein = Protein(sequence)
+if protein.score > -20:
+    best_protein = sim_annealing.experiment(folded_protein, 500, sample_size=2, max_n=10, sim_annealing=True)
+    visualize_protein(best_protein, 3)
+else: print("Score too good:", protein.score)
