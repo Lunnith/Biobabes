88aca7c11d1e4b13efb0932b49bac664a6c9ae9e
Alaya Storm <alayastorm@hotmail.nl>
2023-06-26 15:36:13 +0200

    experiment geimplementeerd voor baseline

--- code/experiments/Randomise_base.py
+++ code/experiments/Randomise_base.py
@@ -7,7 +7,8 @@ from ..classes.aminoacid import Aminoacid
 
 sequence = "HCPHPHPHCHHHHPCCPPHPPPHPPPPCPPPHPPPHPHHHHCHPHPHPHH"
 protein = Protein(sequence)
-best_protein, scores = random_reassignment(protein, 3, k = 10)
+best_protein, scores, best_score = random_reassignment(protein, 3, k = 100000)
 
 print(scores)
+print('best score', best_score)
 visualize_protein(best_protein, 3)
